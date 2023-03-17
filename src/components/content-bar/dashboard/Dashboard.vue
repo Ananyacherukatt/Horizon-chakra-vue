350e03993e1528c08ca52d2b12c16b95eb6a8840
Ananyacherukatt <ananyacherukattu@gmail.com>
2023-03-17 07:01:45 +0530

    first commit

---
+++ src/components/content-bar/dashboard/Dashboard.vue
@@ -0,0 +1,19 @@
+<script setup>
+    import ContentHead from '../content-head/ContentHead.vue';
+    import ContentFoot from '../content-foot/ContentFoot.vue';
+</script>
+
+<template>
+    <div id="dashboard">
+        <ContentHead head="Dashboard"/>
+        
+        <ContentFoot />
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
